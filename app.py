6ef54cf3bdba94b5ab5071aea0c2524e357aa1d0
drtonygenome <147443146+drtonygenome@users.noreply.github.com>
2024-11-16 17:31:07 -1000

    app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_eval_scaled = scaler.transform(X_eval)
 
 #loaded_model = tf.keras.models.load_model("heart_failure_clinical_model")
-loaded_model = keras.layers.TFSMLayer(heart_failure_clinical_model, call_endpoint='serving_default')
+loaded_model = keras.layers.TFSMLayer(heart_failure_clinical_model.keras, call_endpoint='serving_default')
 
 eval_loss, eval_acc = loaded_model.evaluate(X_eval_scaled, y_eval)
 print(
